f7f0e86bdb289d45555a98526588fdd2126d91dc
Fernando Alonzi <58708142+fertech02@users.noreply.github.com>
2023-05-24 18:41:28 +0200

    changed preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,9 +1,28 @@
-from torchvision import datasets, transforms
+import os
+from PIL import Image
+from torchvision import transforms
+from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
 BATCH_SIZE = 128 #number of samples processed at iteration
 IMAGE_SIZE = 64 #size of each image 
 CHANNELS_IMG = 3 #number of channels in a rgb image
+
+class CelebaDataset(Dataset):
+
+    def __init__(self,root_dir,transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.image_list = os.listdir(root_dir)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root_dir, self.image_list[idx])
+        image = Image.open(img_path)
+        if self.transform:
+            image = self.transform(image)
+        return image
+
+
 
 transforms = transforms.Compose(
     [
@@ -13,9 +32,9 @@ transforms = transforms.Compose(
             [0.5 for i in range(CHANNELS_IMG)], [0.5 for i in range(CHANNELS_IMG)]) #ensure that every pixel is in range (-1,1)
     ]
 )
-
+dataset = CelebaDataset(root_dir="../img_align_celeba",transform=transforms)
 #Preprocess the dataset
-dataset = datasets.ImageFolder(root="data/img_align_celeba",transform=transforms)
+#dataset = datasets.ImageFolder(root="../img_align_celeba",transform=transforms)
 
 #Create a dataloader
 data_loader = DataLoader(dataset,batch_size=BATCH_SIZE,shuffle=True)
